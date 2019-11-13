18c4621aea686d411dbd54785fee52fd92d67aa0
The_Number_e <edwardx999@gmail.com>
2019-11-13 00:17:47 -0500

    dumbv3

--- par_malloc.c
+++ par_malloc.c
@@ -7,20 +7,16 @@
 #include <string.h>
 #include "xmalloc.h"
 
-static char* _Atomic data=0;
-static atomic_flag lock=ATOMIC_FLAG_INIT;
-
 void* xmalloc(size_t bytes)
 {
-	if(!atomic_load(&data))
+	static __thread char* data=0;
+	if(data==0)
 	{
-		while(atomic_flag_test_and_set(&lock));
-		char* m=mmap(0,1000000000,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_PRIVATE,-1,0);
-		atomic_store(&data,m);
-		atomic_flag_clear(&lock);
+		data =mmap(0,10000000,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_PRIVATE,-1,0);
 	}
-	size_t* ret=(char*)atomic_fetch_add(&data,bytes+8);
+	size_t* ret=data;
 	*ret=bytes;
+	data+=bytes+8;
 	return ret+1;
 }
 
